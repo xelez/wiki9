f789a16649cd51c43b339f47659b1c63d28ece17
Alex Ankudinov <xelez0@gmail.com>
2013-03-03 16:34:30 +0600

    add support for Flask-Assets

--- wiki9/__init__.py
+++ wiki9/__init__.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from flask import Flask
 from flask.ext.pymongo import PyMongo
 
+from flask.ext.assets import Environment as AssetsEnvironment
+from webassets.loaders import PythonLoader as PythonAssetsLoader
+
 import config
+import assets
+
 
 app = Flask(__name__)
 app.config.from_object(config)
 app.config.from_envvar('WIKI9_SETTINGS', silent=True)
+
+assets_env = AssetsEnvironment(app)
+assets_env.load_path = [os.path.join(app.root_path, 'design')]
+
+#hack because of stupid Flask-Assets, remove as soon as 0.9 released
+assets_env.config['directory'] = assets_env.directory
+assets_env.config['url'] = '/static/'
+
+assets_loader = PythonAssetsLoader(assets)
+for name, bundle in assets_loader.load_bundles().iteritems():
+    assets_env.register(name, bundle)
 
 mongo = PyMongo()
 mongo.init_app(app)
---
+++ wiki9/assets.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from flask.ext.assets import Bundle
+
+site_css = Bundle(
+    'css/normalize.css',
+    'css/app.css',
+    output = 'gen/site.css'
+)
+
+head_js = Bundle(
+    'js/vendor/custom.modernizr.js',
+    output = 'gen/head.js',
+)
+
+vendor_js = Bundle(
+    'js/vendor/jquery.js',
+    'js/vendor/jquery.cookie.js',
+)
+
+foundation_js = Bundle(
+    'js/foundation/foundation.js',
+    'js/foundation/foundation.orbit.js',
+    'js/foundation/foundation.placeholder.js',
+    'js/foundation/foundation.alerts.js',
+    'js/foundation/foundation.reveal.js',
+    'js/foundation/foundation.tooltips.js',
+    'js/foundation/foundation.topbar.js',
+    'js/foundation/foundation.cookie.js',
+    'js/foundation/foundation.section.js',
+    'js/foundation/foundation.joyride.js',
+    'js/foundation/foundation.forms.js',
+    'js/foundation/foundation.dropdown.js',
+    'js/foundation/foundation.clearing.js',
+    'js/foundation/foundation.magellan.js',
+)
+
+all_js = Bundle(
+    vendor_js,
+    foundation_js,
+    output = 'gen/all.js'
+)
+
