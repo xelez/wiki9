ab0dbc72e6837f1e44eadc9de1631a94e466d142
Alex Ankudinov <xelez0@gmail.com>
2013-03-03 15:17:46 +0600

    Little improvement in wiki9.js

--- wiki9/static/js/wiki9.js
+++ wiki9/static/js/wiki9.js
@@ -1,17 +1,19 @@
-// Highlight current nav
-$('#sidenav ul ul').hide();
-myurl = location.href;
-$('#sidenav ul li a').filter(function() {
-    return myurl.indexOf($(this)[0].href) == 0;
-}).closest('li').addClass('active').children('ul').show();
+$(function() {
+    // Highlight current nav
+    $('#sidenav ul ul').hide();
+    myurl = location.href;
+    $('#sidenav ul li a').filter(function() {
+        return myurl.indexOf($(this)[0].href) == 0;
+    }).closest('li').addClass('active').children('ul').show();
 
 
-// Close alerts on click
-$(".alert-box").click(function() {
-    $(this).hide(); 
-});
+    // Close alerts on click
+    $(".alert-box").click(function() {
+        $(this).hide(); 
+    });
 
-$(".alert-container").hide().fadeIn('slow').delay(5000).fadeOut('slow');
+    $(".alert-container").hide().fadeIn('slow').delay(5000).fadeOut('slow');
+})
 
 // Validation config TODO: rewrite or delete
 /* $.validator.addMethod(
