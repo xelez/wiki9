1dc10049179ad505fe5aee7b78102fc7190c5ee0
Alex Ankudinov <xelez0@gmail.com>
2013-03-06 18:05:01 +0600

    Move out that unneeded hack

--- wiki9/__init__.py
+++ wiki9/__init__.py
@@ -17,12 +17,6 @@ app.config.from_object(config)
 app.config.from_envvar('WIKI9_SETTINGS', silent=True)
 
 assets_env = AssetsEnvironment(app)
-assets_env.load_path = [os.path.join(app.root_path, 'design')]
-
-#hack because of stupid Flask-Assets, remove as soon as 0.9 released
-assets_env.config['directory'] = assets_env.directory
-assets_env.config['url'] = '/static/'
-
 assets_loader = PythonAssetsLoader(assets)
 for name, bundle in assets_loader.load_bundles().iteritems():
     assets_env.register(name, bundle)
