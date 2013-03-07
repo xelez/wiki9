fbfe55aa4d44ca60a166ba4be9c9b5cc0341c12a
Alex Ankudinov <xelez0@gmail.com>
2013-03-07 16:00:05 +0600

    New assets

--- wiki9/assets.py
+++ wiki9/assets.py
@@ -3,42 +3,19 @@
 from flask.ext.assets import Bundle
 
 site_css = Bundle(
-    'css/normalize.css',
+    'vendor/bootstrap/css/bootstrap.css',
     'css/app.css',
     'css/content.css',
     output = 'gen/site.css'
 )
 
-head_js = Bundle(
-    'js/vendor/custom.modernizr.js',
-    output = 'gen/head.js',
-)
-
 vendor_js = Bundle(
-    'js/vendor/jquery.js',
-    'js/vendor/jquery.cookie.js',
-)
-
-foundation_js = Bundle(
-    'js/foundation/foundation.js',
-    'js/foundation/foundation.orbit.js',
-    'js/foundation/foundation.placeholder.js',
-    'js/foundation/foundation.alerts.js',
-    'js/foundation/foundation.reveal.js',
-    'js/foundation/foundation.tooltips.js',
-    'js/foundation/foundation.topbar.js',
-    'js/foundation/foundation.cookie.js',
-    'js/foundation/foundation.section.js',
-    'js/foundation/foundation.joyride.js',
-    'js/foundation/foundation.forms.js',
-    'js/foundation/foundation.dropdown.js',
-    'js/foundation/foundation.clearing.js',
-    'js/foundation/foundation.magellan.js',
+    'vendor/jquery/jquery-1.9.1.js',
+    'vendor/bootstrap/js/bootstrap.js',
 )
 
 all_js = Bundle(
     vendor_js,
-    foundation_js,
     output = 'gen/all.js'
 )
 
