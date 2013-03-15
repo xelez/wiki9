de5cf2fe65dfd686de38068e4f78f0a4d0905ba7
Alex Ankudinov <xelez0@gmail.com>
2013-03-15 09:27:50 +0600

    Add base Flask-Admin

--- wiki9/__init__.py
+++ wiki9/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
+import os.path
 
 from flask import Flask
 from flask.ext.pymongo import PyMongo
@@ -8,18 +8,22 @@ from flask.ext.pymongo import PyMongo
 from flask.ext.assets import Environment as AssetsEnvironment
 from webassets.loaders import PythonLoader as PythonAssetsLoader
 
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.fileadmin import FileAdmin
+
 import config
-import assets
-
-
 app = Flask(__name__)
 app.config.from_object(config)
 app.config.from_envvar('WIKI9_SETTINGS', silent=True)
 
+import assets
 assets_env = AssetsEnvironment(app)
 assets_loader = PythonAssetsLoader(assets)
 for name, bundle in assets_loader.load_bundles().iteritems():
     assets_env.register(name, bundle)
+
+admin = Admin(app);
+admin.add_view(FileAdmin(app.static_folder, '/static/', name='Static Files'))
 
 mongo = PyMongo()
 mongo.init_app(app)
