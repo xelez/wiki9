630a2b47bff0f789e970c52e04f26c1e01d636ad
Alex Ankudinov <xelez0@gmail.com>
2013-03-19 20:11:07 +0600

    Use paths from config instead of hard-written

--- wiki9/file_views.py
+++ wiki9/file_views.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 from wiki9 import app
 
-## temp; server files from storage
 from flask import send_from_directory
+from os import path
+## temp; server files from storage
+
 @app.route('/files/<path:filename>')
-def send_storages(filename):
-     return send_from_directory('/home/xelez/programming/wiki9/_data/files/', filename)
+def send_files(filename):
+    directory = path.join(app.config['FILE_DIRECTORY'], 'files')
+    return send_from_directory(directory, filename)
 
 @app.route('/storages/<path:filename>')
 def send_storages(filename):
-     return send_from_directory('/home/xelez/programming/wiki9/_data/storages/', filename)
+    directory = path.join(app.config['FILE_DIRECTORY'], 'storages')
+    return send_from_directory(directory, filename)
 
 @app.route('/pics/<path:filename>')
 def send_pics(filename):
-     return send_from_directory('/home/xelez/programming/wiki9/_data/pics/', filename)
+    directory = path.join(app.config['FILE_DIRECTORY'], 'pics')
+    return send_from_directory(directory, filename)
 
